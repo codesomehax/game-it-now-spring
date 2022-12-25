cde92d767ce2fbfb9df85dc153d6508c3efe8517
MARKIEWICZ Dariusz <dariusz.markiewicz@idemia.com>
2022-12-25 17:17:21 +0100

    Initial commit

---
+++ src/main/java/pl/lodz/uni/edu/gin/GameItNowApplication.java
@@ -0,0 +1,13 @@
+package pl.lodz.uni.edu.gin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameItNowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameItNowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/lodz/uni/edu/gin/GameItNowApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lodz.uni.edu.gin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameItNowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
